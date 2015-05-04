57837ecc715c8bac8a7157dcf7025f5364bffffb
Weyjones <Crthas@Weizong.local>
2015-05-04 13:59:27 -0700

    dsddd

--- fristJS.js
+++ fristJS.js
@@ -7,10 +7,10 @@ console.log("Before my alert");
 alert("Welcome to my first Javascript"); 
  num1=prompt("What's your frist number?");
  num2=prompt("What's your second number?")
-if (num1<num2){
+if(num1<num2){
   console.log("num2 is bigger");
   alert(num2+" is greater than"+num1); 
-  
+  }
 else if(num1>num2){ 
   console.log("num1 is bigger");
   alert(num1+" is greater than"+num2);}
