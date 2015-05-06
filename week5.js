b4079fac2eaf8d068b836408368a4a7a737e3e3c
Weyjones <Crthas@Weizong.local>
2015-05-06 14:14:31 -0700

    ddd

--- week5.js
+++ week5.js
@@ -1,12 +1,14 @@
+function count(){
 var num;
 var i = 0;       // Set counter to 1
 var msg = '';    // Message
 
 // Store 5 times table in a variable
-num=prompt("Enter a number to generate the multiplication table");
+num=document.getElementById("intNumber").value;
 while (i < 11) {
   msg += i + ' x '+num+' = ' + (i * num) + '<br />';
   i++;
 }
 
 document.getElementById('answer').innerHTML = msg;
+}
