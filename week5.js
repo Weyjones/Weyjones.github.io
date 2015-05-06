fe7c19375a6dd03df25ae2dca1b69c5e1bac6a5c
Weyjones <Crthas@Weizong.local>
2015-05-06 13:56:34 -0700

    ddd

--- week5.js
+++ week5.js
@@ -5,7 +5,7 @@ var msg = '';    // Message
 // Store 5 times table in a variable
 num=prompt("Enter a number to generate the multiplication table");
 while (i < 11) {
-  msg += i + ' x num = ' + (i * num) + '<br />';
+  msg += i + ' x '+num+' = ' + (i * num) + '<br />';
   i++;
 }
 
